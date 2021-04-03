5a73f8f1a7885289dd20638019ea1882d6eab9a9
Sean <seancasey076@gmail.com>
2021-04-03 11:56:31 -0500

    header added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+import React, { Component } from 'react';
+import Header from './components/Header';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+      <header>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
+            integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
+            crossorigin="anonymous"></link>
       </header>
-    </div>
-  );
+        <Header />
+        <p>Sean Casey</p>
+      </div>
+    )
+  }
 }
+
 
 export default App;
