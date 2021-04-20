ba931329969e9e7ea1ef56d134b17a1554f9984b
Sean <seancasey076@gmail.com>
2021-04-20 18:44:22 -0500

    working on contact page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -3,7 +3,31 @@ import { Component } from 'react';
 export default class Contact extends Component {
     render() {
         return(
-            <p>Contact works</p>
+           <div>    
+                <form className="form-container">
+                    <div className="form-group">
+                    <label for="emailInput">Email Address:</label>
+                        <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Email" />
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label for="firstName">First Name:</label>
+                        <input type="text" className="form-control" id="firstName" placeholder="First Name" />
+                    </div>
+                    <div className="form-group">
+                        <label for="lastName">Last Name:</label>
+                        <input type="text" className="form-control" id="lastName" placeholder="Last Name" />
+                    </div>
+                    <div className="form-group">
+                        <label for="responseArea">Enter Response:</label>
+                        <textarea className="form-control" id="responseArea" rows="5"></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+        
+               
+            </div>
+            
         )
     }
 }
--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -5,9 +5,10 @@ export default class Projects extends Component {
         return(
             <div className="footerContainer">
                 <footer>
-                    <p className="footer" ><i className="fas fa-copyright"></i>Sean Casey 2021</p>
-                    <a href="https://github.com/caseyDev422" target="_blank" rel="noopener noreferrer"><i className="fab fa-github githubIcon"></i></a> <a
-                    target="_blank" rel="noopener noreferrer" 
+                    <p className="footer" ><i className="fas fa-copyright"></i> Sean Casey 2021</p>
+                    <a href="https://github.com/caseyDev422" target="_blank" rel="noopener noreferrer"><i className="fab fa-github githubIcon"></i></a> 
+                    
+                    <a target="_blank" rel="noopener noreferrer" 
                     href="https://www.linkedin.com/in/sean-casey-b0b1ba170?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BN59q9dZ3S%2BSjT1eemnbByA%3D%3D">
                     <i class="fab fa-linkedin"></i></a>
                 </footer>
